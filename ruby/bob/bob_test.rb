b4e2b99be35859dfc2fccc42d1167ac2cabc1f46
Zakhi Verech <zakhiv@gmail.com>
2014-02-18 09:04:43 +0200

    ruby/bob (#1)

--- ruby/bob/bob_test.rb
+++ ruby/bob/bob_test.rb
@@ -25,82 +25,66 @@ class TeenagerTest < MiniTest::Unit::TestCase
   end
 
   def test_shouting
-    skip
     assert_equal 'Woah, chill out!', teenager.hey('WATCH OUT!')
   end
 
   def test_asking_a_question
-    skip
     assert_equal 'Sure.', teenager.hey('Does this cryogenic chamber make me look fat?')
   end
 
   def test_asking_a_numeric_question
-    skip
     assert_equal 'Sure.', teenager.hey('You are, what, like 15?')
   end
 
   def test_talking_forcefully
-    skip
     assert_equal 'Whatever.', teenager.hey("Let's go make out behind the gym!")
   end
 
   def test_using_acronyms_in_regular_speech
-    skip
     assert_equal 'Whatever.', teenager.hey("It's OK if you don't want to go to the DMV.")
   end
 
   def test_forceful_questions
-    skip
     assert_equal 'Woah, chill out!', teenager.hey('WHAT THE HELL WERE YOU THINKING?')
   end
 
   def test_shouting_numbers
-    skip
     assert_equal 'Woah, chill out!', teenager.hey('1, 2, 3 GO!')
   end
 
   def test_only_numbers
-    skip
     assert_equal 'Whatever.', teenager.hey('1, 2, 3')
   end
 
   def test_question_with_only_numbers
-    skip
     assert_equal 'Sure.', teenager.hey('4?')
   end
 
   def test_shouting_with_special_characters
-    skip
     assert_equal 'Woah, chill out!', teenager.hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!')
   end
 
   def test_shouting_with_no_exclamation_mark
-    skip
     assert_equal 'Woah, chill out!', teenager.hey('I HATE YOU')
   end
 
   def test_statement_containing_question_mark
-    skip
     assert_equal 'Whatever.', teenager.hey('Ending with ? means a question.')
   end
 
   def test_prattling_on
-    skip
     assert_equal 'Sure.', teenager.hey("Wait! Hang on. Are you going to be OK?")
   end
 
   def test_silence
-    skip
     assert_equal 'Fine. Be that way!', teenager.hey('')
   end
 
   def test_prolonged_silence
-    skip
     assert_equal 'Fine. Be that way!', teenager.hey('    ')
   end
 
   def test_on_multiple_line_questions
-    skip
     assert_equal 'Whatever.', teenager.hey(%{
 Does this cryogenic chamber make me look fat?
 no})
