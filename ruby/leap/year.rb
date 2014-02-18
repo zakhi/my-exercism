093472980dc236c066a3d3a83451e8801d70f46b
Zakhi Verech <zakhiv@gmail.com>
2014-02-18 13:06:59 +0200

    ruby/leap (#3) Numeric patch

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,13 +1,14 @@
+class Numeric
+  def divisible_by?(divisor)
+    self % divisor == 0
+  end
+end
+
 module Year
   extend self
 
   def leap?(year)
-    divisible_by?(year, 4) && (!divisible_by?(year, 100) || divisible_by?(year, 400))
+    year.divisible_by?(4) && (!year.divisible_by?(100) || year.divisible_by?(400))
   end
 
-private
-
-  def divisible_by?(year, divisor)
-    year % divisor == 0
-  end
 end
