b60ec21787a6c96fc9c5ca57e2bfdcd690446197
Zakhi Verech <zakhiv@gmail.com>
2014-02-19 16:28:56 +0200

    ruby/scrabble-score (#2) reorder

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,14 +1,5 @@
 class Scrabble
 
-  def self.create_letter_values(array)
-    array.each_slice(2).each_with_object({}) do |(letters, value), hash|
-      letters.chars.each { |l| hash[l] = value }
-    end
-  end
-
-  VALUES = create_letter_values ["aeioulnrst", 1, "dg", 2, "bcmp", 3, 
-                                 "fhvwy", 4, "k", 5, "jx", 8, "qz", 10]
-
   def self.score(word)
     Scrabble.new(word).score
   end
@@ -19,14 +10,34 @@ class Scrabble
 
   def score
     score_hash.reduce(0) do |score, (letter, count)|
-      score + count * VALUES[letter]
+      score + count * value_of(letter)
     end
   end
 
+private 
+
+  def self.create_letter_values(array)
+    array.each_with_object({}) do |(letters, value), hash|
+      letters.chars.each { |l| hash[l] = value }
+    end
+  end
+
+  VALUES = create_letter_values [["aeioulnrst", 1], 
+                                 ["dg",         2], 
+                                 ["bcmp",       3], 
+                                 ["fhvwy",      4], 
+                                 ["k",          5], 
+                                 ["jx",         8], 
+                                 ["qz",        10]]
+
   def score_hash
-    @word.strip.chars.each_with_object(Hash.new(0)) do |char, hash|
+    @word.chars.each_with_object(Hash.new(0)) do |char, hash|
       hash[char.downcase] += 1
     end
+  end
+
+  def value_of(letter)
+    VALUES[letter] || 0
   end
 
 end
