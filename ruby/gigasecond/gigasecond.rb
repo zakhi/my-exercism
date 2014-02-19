b2d14747413fab383170fa8e14d4c63291727883
Zakhi Verech <zakhiv@gmail.com>
2014-02-19 16:46:24 +0200

    ruby/gigasecond (#1) small renaming

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,13 +1,13 @@
 class Gigasecond
 
-  SECOND_IN_DAY = 24 * 60 * 60
+  SECONDS_IN_DAY = 24 * 60 * 60
 
   def initialize(date)
     @date = date
   end
 
   def date
-    @date + (10 ** 9 / SECOND_IN_DAY)
+    @date + (10 ** 9 / SECONDS_IN_DAY)
   end
 
 end
