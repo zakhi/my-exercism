21a0b6498fa35ba1917091d6240e7aca286d56c8
Zakhi Verech <zakhiv@gmail.com>
2014-02-20 10:24:44 +0200

    ruby/anagram (#2) extracting methods

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,19 +1,27 @@
 class Anagram
   def initialize(word)
-    @word = word
-    @letters = ordered_letters(@word)
+    @word = word || ""
+    @letters = letters_in_order(@word)
   end
 
   def match(candidates)
     candidates.find_all do |candidate|
-      @word.casecmp(candidate) != 0 && ordered_letters(candidate) == @letters
+      ! same_word?(candidate) && anagram_of?(candidate)
     end
   end
 
 private
 
-  def ordered_letters(string)
-    string.downcase.chars.sort
+  def letters_in_order(word)
+    word.downcase.chars.sort
+  end
+
+  def same_word?(word)
+    @word.casecmp(word) == 0
+  end
+
+  def anagram_of?(word)
+    @letters == letters_in_order(word)
   end
 
 end
