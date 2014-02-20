742db1906b19b45c0fe3ca07f4e2b72eb1a5b6e4
Zakhi Verech <zakhiv@gmail.com>
2014-02-20 15:16:45 +0200

    ruby/space-age (#3) using define_method

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -8,22 +8,15 @@ class SpaceAge
     @seconds
   end
 
-  def method_missing(method, *args, &block)
-    if method.to_s =~ /on_(.+)/ && args.empty? && RELATIVE_PERIOD[$1.to_sym]
-      on_planet($1.to_sym)
-    else
-      super
-    end
+  period_factors = { earth: 1.0, mercury: 0.2408467, venus: 0.61519726, mars: 1.8808158,
+                     jupiter: 11.862615, saturn: 29.447498, uranus: 84.016846, neptune: 164.79132 }
+
+
+  period_factors.each do |planet, factor|
+    define_method("on_#{planet}".to_sym) { (earth_years / factor).round(2) }
   end
 
 private
-
-  RELATIVE_PERIOD = { earth: 1.0, mercury: 0.2408467, venus: 0.61519726, mars: 1.8808158,
-                      jupiter: 11.862615, saturn: 29.447498, uranus: 84.016846, neptune: 164.79132 }
-
-  def on_planet(planet)
-    (earth_years / RELATIVE_PERIOD[planet]).round(2)
-  end
 
   def earth_years
     @seconds / 31557600.0
