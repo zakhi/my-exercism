5f75c9943221076280a56d68b1273de4eaae215d
Zakhi Verech <zakhiv@gmail.com>
2014-02-23 17:36:28 +0200

    ruby/triangle (#2) clearer validation

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -4,17 +4,22 @@ class Triangle
 
   def initialize(*sides)
     @sides = sides
+    assert_valid
   end
 
   def kind
-    assert_valid_triangle
     [:equilateral, :isosceles, :scalene][@sides.uniq.count - 1]
   end
 
 private
 
-  def assert_valid_triangle
-    sum = @sides.inject(:+)
-    raise TriangleError if @sides.find { |s| s <= 0 || s >= sum / 2.0 }
+  def assert_valid
+    side_limit = perimeter / 2.0
+    raise TriangleError if @sides.find { |side| side <= 0 || side >= side_limit }
   end
+
+  def perimeter
+    @sides.inject(:+)
+  end
+
 end
