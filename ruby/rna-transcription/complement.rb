abfa7c5aa639fea3c954c27abd496b076ec6e8cf
Zakhi Verech <zakhiv@gmail.com>
2014-02-25 09:19:45 +0200

    ruby/rna-transcription (#2) remove duplication

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,15 +1,19 @@
 module Complement
   extend self
 
-  RNA_TO_DNA = { "G" => "C", "C" => "G", "T" => "A", "A" => "U" }
-  DNA_TO_RNA = { "G" => "C", "C" => "G", "U" => "A", "A" => "T" }
+  DNA_TO_RNA = { "G" => "C", "C" => "G", "T" => "A", "A" => "U" }
 
   def of_dna(dna)
-    dna.each_char.map { |n| RNA_TO_DNA[n] }.join
+    complement dna, DNA_TO_RNA
   end
 
   def of_rna(rna)
-    rna.each_char.map { |n| DNA_TO_RNA[n] }.join
+    complement rna, DNA_TO_RNA.invert
   end
 
+private
+
+  def complement(sequence, map)
+    sequence.each_char.map { |n| map[n] }.join
+  end
 end
