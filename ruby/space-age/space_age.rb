ecf8caf050699b5e1e25b125c530c368475d3d13
Zakhi Verech <zakhiv@gmail.com>
2014-02-26 08:19:00 +0200

    ruby/space-age (#5) clearer factors

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,15 +1,18 @@
 class SpaceAge
+  attr_reader :seconds
 
   def initialize(seconds)
     @seconds = seconds
   end
 
-  def seconds
-    @seconds
-  end
-
-  period_factors = { earth: 1.0, mercury: 0.2408467, venus: 0.61519726, mars: 1.8808158,
-                     jupiter: 11.862615, saturn: 29.447498, uranus: 84.016846, neptune: 164.79132 }
+  period_factors = { earth:   1.0, 
+                     mercury: 0.2408467, 
+                     venus:   0.61519726, 
+                     mars:    1.8808158,
+                     jupiter: 11.862615, 
+                     saturn:  29.447498, 
+                     uranus:  84.016846, 
+                     neptune: 164.79132 }
 
 
   period_factors.each do |planet, factor|
@@ -19,6 +22,6 @@ class SpaceAge
 private
 
   def earth_years
-    @earth_years ||= @seconds / 31557600.0
+    @earth_years ||= @seconds / (365.25 * 24 * 60 * 60)
   end
 end
