251f13cd8e35f188a6c5fb2108db438abfc68c12
Zakhi Verech <zakhiv@gmail.com>
2014-03-29 22:53:05 +0300

    ruby/ocr-numbers (#2) refactoring

--- ruby/ocr-numbers/ocr.rb
+++ ruby/ocr-numbers/ocr.rb
@@ -4,19 +4,22 @@ class OCR
   end
 
   def convert
-    @text.lines.each_slice(4).map { |line| convert_line(line) }.join ","
+    text_lines.map { |line| convert_line(line) }.join ","
   end
 
 private
+  def text_lines
+    @text.split("\n").each_slice(4)
+  end
+
   def convert_line(line)
     self.class.digits(line).map { |digit| NUMBERS[digit] }.join
   end
 
-  def self.digits(text)
-    text.each_with_object([]) do |line, result|
-      line.scan(/.{3}/).each_with_index do |part, index|
-        result[index] ||= []
-        result[index] << part
+  def self.digits(rows)
+    rows.each_with_object([]) do |row, result|
+      row.chars.each_slice(3).with_index do |digit_row, index|
+        (result[index] ||= []) << digit_row
       end
     end
   end
@@ -29,7 +32,7 @@ private
   END
 
   NUMBERS = Hash.new("?").tap do |hash|
-    digits(numbers.lines).map.with_index { |code, index| hash[code] = index }
+    digits(numbers.split "\n").map.with_index { |code, index| hash[code] = index }
   end
 
 end
